4bebbb4e4fadabd8c227a213231cc2997f724264
Maicol Bernal <bernalmaicolw@gmail.com>
2025-09-11 16:07:20 -0500

    Agregué la sección de Reviews con sus respectivos estilos

--- js/main.js
+++ js/main.js
@@ -25,3 +25,31 @@ $(document).ready(function () {
     });
 });
 
+
+$(document).ready(function () {
+    $('.slider-reviews').slick({
+        slidesToShow: 3,   // muestras 4 columnas al mismo tiempo
+        slidesToScroll: 1, // avanzas de a 1
+        infinite: true,    // loop infinito
+        speed: 300,
+        arrows: true,      // flechas
+        dots: false,        // bolitas de navegación
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 992, // tablets y pantallas medianas
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576, // móviles
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
